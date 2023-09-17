4293fe7b2bba837e8a2eadfd5deec4db64335aee
oruma22 <leroyorusgmail.com>
2023-09-17 12:35:54 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks if it is both lower or upper case
+ * @c: parameter to be checked
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
